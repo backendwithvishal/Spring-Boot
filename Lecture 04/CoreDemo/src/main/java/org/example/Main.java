51cdb331a06052109241a6cc88a8f7699665fb5f
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-18 21:07:37 +0530

    Commit added in Main file

--- Lecture 04/CoreDemo/src/main/java/org/example/Main.java
+++ Lecture 04/CoreDemo/src/main/java/org/example/Main.java
@@ -8,15 +8,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        NotificationService notification = new FakeEmailService();
+        NotificationService notification = new EmailService();
 
 //        OrderService order = new OrderService(notification);
 
         OrderService order = new OrderService();
-
+        order.setNotification(notification);
         order.placOrder();
     }
 }
 
 // A class should ask what it needs and not
 // build everything itself
+
+// IDC --> Inversion of Control
