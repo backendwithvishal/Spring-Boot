62f164f0071eb262fbc18f5c66fb5a9424275292
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-22 19:54:00 +0530

    Code added in new files

--- Lecture 07/CircularDependencyDemo/src/main/java/vishal/org/OrderService.java
+++ Lecture 07/CircularDependencyDemo/src/main/java/vishal/org/OrderService.java
@@ -1,4 +1,19 @@
 package vishal.org;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderService {
+
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public void placeOrder(){
+        paymentService.pay();
+
+        System.out.println("Order placed");
+    }
 }
--- Lecture 07/CircularDependencyDemo/src/main/java/vishal/org/PaymentService.java
+++ Lecture 07/CircularDependencyDemo/src/main/java/vishal/org/PaymentService.java
@@ -1,4 +1,10 @@
 package vishal.org;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PaymentService {
+    public void pay() {
+        System.out.println("Payment done");
+    }
 }
