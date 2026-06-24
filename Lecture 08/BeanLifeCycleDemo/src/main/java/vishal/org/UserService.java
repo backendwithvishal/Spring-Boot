6d60b091d865ee34d623b3d1d24dd70f027194cf
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-24 19:24:17 +0530

    small update in User service code

--- Lecture 08/BeanLifeCycleDemo/src/main/java/vishal/org/UserService.java
+++ Lecture 08/BeanLifeCycleDemo/src/main/java/vishal/org/UserService.java
@@ -1,11 +1,18 @@
 package vishal.org;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserService {
+public class UserService implements BeanNameAware {
 
     public UserService() {
         System.out.println("UserService constructor called");
     }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean name is " + name );
+    }
+
 }
