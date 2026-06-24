30d5b2b833e5cfd7afd4f6297989fceb3e1d2a67
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-24 17:46:48 +0530

    small update in codebase

--- Lecture 08/BeanLifeCycleDemo/src/main/java/vishal/org/OrderService.java
+++ Lecture 08/BeanLifeCycleDemo/src/main/java/vishal/org/OrderService.java
@@ -1,14 +1,17 @@
 package vishal.org;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderService {
+
+//    @Autowired
     private PaymentService paymentService;
 
-    public  OrderService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public  OrderService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 
     public void placeOrder(){
         System.out.println("Order palced");
