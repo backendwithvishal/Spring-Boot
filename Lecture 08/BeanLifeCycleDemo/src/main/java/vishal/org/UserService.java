f4214dc856e0ce67c01d1cd96cca9163959f699c
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-24 19:38:24 +0530

    small update in codebase

--- Lecture 08/BeanLifeCycleDemo/src/main/java/vishal/org/UserService.java
+++ Lecture 08/BeanLifeCycleDemo/src/main/java/vishal/org/UserService.java
@@ -1,18 +1,30 @@
 package vishal.org;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserService implements BeanNameAware {
+public class UserService implements BeanNameAware, ApplicationContextAware {
 
     public UserService() {
         System.out.println("UserService constructor called");
     }
 
+    // Callback method
     @Override
     public void setBeanName(String name) {
-        System.out.println("Bean name is " + name );
+        System.out.println("Bean name is " + name);
     }
 
+    public String getBean() {
+        return "userBean";
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("ApplicationContext name is " + applicationContext.getClass());
+    }
 }
