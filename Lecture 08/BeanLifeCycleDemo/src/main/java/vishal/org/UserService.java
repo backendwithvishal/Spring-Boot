b25a8827ad82f6c48b5ff83b1cb19d66d73d4078
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-24 19:15:57 +0530

    User service file created and code added

---
+++ Lecture 08/BeanLifeCycleDemo/src/main/java/vishal/org/UserService.java
@@ -0,0 +1,11 @@
+package vishal.org;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+
+    public UserService() {
+        System.out.println("UserService constructor called");
+    }
+}
